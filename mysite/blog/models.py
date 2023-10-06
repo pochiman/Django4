f5125f485d417cea6efed00c37b6fa8ddec09a13
pochiman <pochiman@yahoo.com>
2023-10-05 20:45:50 -0400

    initial commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
-            .filter(status=Post.Status.PUBLISHED)
+                      .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
 
@@ -14,7 +15,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, 
+                            unique_for_date='publish')
     author = models.ForeignKey(User, 
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -36,3 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', 
+                       args=[self.publish.year, 
+                             self.publish.month, 
+                             self.publish.day, 
+                             self.slug])

9c21f6c80fd86d9656fb2a9fd1f46fe3c0107332
pochiman <pochiman@yahoo.com>
2023-10-22 23:15:59 -0400

    initial commit

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -8,7 +8,7 @@ ADMINS = [
     ('Pönch T', 'email@mydomain.com'),
 ]
 
-ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+ALLOWED_HOSTS = ['.educaproject.com']
 
 DATABASES = {
    'default': {
